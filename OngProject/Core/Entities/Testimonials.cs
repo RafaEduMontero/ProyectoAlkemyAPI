eebdf7096202cae7964328ff6e96ecbfc8477c4d
EzeVillo <73943327+EzeVillo@users.noreply.github.com>
2021-09-28 17:18:45 -0300

    Modelo Testimonials

---
+++ OngProject/Core/Entities/Testimonials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Entities
+{
+    public class Testimonials : EntityBase
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Content { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}

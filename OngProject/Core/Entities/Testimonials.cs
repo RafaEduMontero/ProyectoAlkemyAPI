01efb8007f9043294772335b78a55c630da0217a
EzeVillo <73943327+EzeVillo@users.noreply.github.com>
2021-09-28 17:22:49 -0300

    Correccion cambios no guardados

--- OngProject/Core/Entities/Testimonials.cs
+++ OngProject/Core/Entities/Testimonials.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,17 @@ namespace OngProject.Core.Entities
 {
     public class Testimonials : EntityBase
     {
-        public string Name { get; set; }
-        public string Image { get; set; }
-        public int Content { get; set; }
-        public DateTime DeletedAt { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(255)")]
+        [MaxLength(255)]
+        public string name { get; set; }
+
+        [Column(TypeName = "VARCHAR(255)")]
+        [MaxLength(255)]
+        public string image { get; set; }
+
+        [Column(TypeName = "VARCHAR(65535)")]
+        [MaxLength(65535)]
+        public int content { get; set; }
     }
 }

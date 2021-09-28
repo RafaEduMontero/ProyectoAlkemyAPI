96aeae1f708b788820253034693bdd07505fb277
Alejandro Jacobo <thesenntinel@outlook.com>
2021-09-28 14:14:14 -0300

    Se agrego EntityBase junto con ApplicationDbContext (falta agregar en el conection string el nombre de la Db)

---
+++ OngProject/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("data source = Localhost; " +
+                                                 "initial catalog = ; " +
+                                                 "Integrated Security = true");
+        }
+
+    }
+}

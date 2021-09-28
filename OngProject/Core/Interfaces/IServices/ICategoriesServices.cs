b6ec2383d8dbe7b32e69c388c642932ce3d8721a
Alejandro Jacobo <thesenntinel@outlook.com>
2021-09-28 19:28:45 -0300

    Crear repositorio, controlador y servicios de Categories

---
+++ OngProject/Core/Interfaces/IServices/ICategoriesServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces.IServices
+{
+    public interface ICategoriesServices
+    {
+
+
+    }
+}

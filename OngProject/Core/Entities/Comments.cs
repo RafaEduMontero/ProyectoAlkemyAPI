1b8d791ecb8eeedd863cd2f4951bfaf4a586270e
EzeVillo <73943327+EzeVillo@users.noreply.github.com>
2021-09-29 16:35:39 -0300

    Modelo Comments

---
+++ OngProject/Core/Entities/Comments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Entities
+{
+    public class Comments : EntityBase
+    {
+        [Required]
+        [Column(TypeName = "INTEGER")]
+        [ForeignKey("UserId")]
+        public int User_id { get; set; }
+
+        [Required]
+        [Column(TypeName = "TEXT")]
+        [MaxLength(4000)]
+        public string Body { get; set; }
+
+        [Required]
+        [Column(TypeName = "INTEGER")]
+        [ForeignKey("NewId")]
+        public int New_id { get; set; }
+    }
+}

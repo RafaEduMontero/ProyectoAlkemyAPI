3a80dfd8bf4552640c0d12b1241b0adbed5bd06a
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-29 21:16:28 -0300

    Agregando la Entidad : Slides

---
+++ OngProject/Core/Entities/Slides.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Entities
+{
+    public class Slides : EntityBase
+    {
+        [Required]
+        [Column(TypeName = "VARCHAR(255)")]
+        public string ImageUrl { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(4000)")]
+        public string Text { get; set; }
+        [Required]
+        public int Order { get; set; }
+        //Foreign Key hacia una organizacion
+        public int OrganizationId { get; set; }
+        [ForeignKey("OrganizationId")]
+        public virtual Organizations organizations { get; set; }
+    }
+}

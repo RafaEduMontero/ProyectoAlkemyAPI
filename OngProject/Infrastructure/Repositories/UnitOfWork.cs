f5e37450ad0c72ab7b96925c7599c2a246044a3a
Rafael Montero <rafaedumontero@gmail.com>
2021-09-30 10:46:27 -0300

    repositorio, controlador y servicios de Roles creados

--- OngProject/Infrastructure/Repositories/UnitOfWork.cs
+++ OngProject/Infrastructure/Repositories/UnitOfWork.cs
@@ -19,14 +19,16 @@ namespace OngProject.Infrastructure.Repositories
 
         private readonly IBaseRepository<Organizations> _organizationsRepository;
         private readonly IBaseRepository<Member> _memberRepository;
+        private readonly IBaseRepository<Roll> _rollRepository;
 
         public IBaseRepository<Organizations> NewsRepository => _organizationsRepository ?? new BaseRepository<Organizations>(_context);
-        public IBaseRepository<Member> NewsRpository => _memberRepository ?? new BaseRepository<Member>(_context);
+        public IBaseRepository<Member> MemberRepository => _memberRepository ?? new BaseRepository<Member>(_context);
 
 
         private readonly IBaseRepository<Activities> _ActivitiesRepository;
 
         public IBaseRepository<Activities> ActivitiesRepository => _ActivitiesRepository ?? new BaseRepository<Activities>(_context);
+        public IBaseRepository<Roll> RollRepository => _rollRepository ?? new BaseRepository<Roll>(_context);
 
         public void Dispose()
         {

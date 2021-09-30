41c993c47c07106010361d5f8cfba072bfc39ea2
Alejandro Jacobo <thesenntinel@outlook.com>
2021-09-30 12:07:56 -0300

    Se quita metodo provisional en entity/category. Se quita la clase CategoriesRepository, para agregar el repositorio a UnitOfWork

--- OngProject/Infrastructure/Repositories/UnitOfWork.cs
+++ OngProject/Infrastructure/Repositories/UnitOfWork.cs
@@ -18,8 +18,13 @@ namespace OngProject.Infrastructure.Repositories
         }
 
         private readonly IBaseRepository<Organizations> _organizationsRepository;
+        private readonly IBaseRepository<Category> _categoryRepository;
         private readonly IBaseRepository<Testimonials> _testimonialsRepository;
+        private readonly IBaseRepository<Activities> _ActivitiesRepository;
+        private readonly IBaseRepository<Member> _memberRepository;
+
         public IBaseRepository<Organizations> NewsRepository => _organizationsRepository ?? new BaseRepository<Organizations>(_context);
+        public IBaseRepository<Category> CategoriesRepository => _categoryRepository ?? new BaseRepository<Category>(_context);
         public IBaseRepository<Testimonials> TestimonialsRepository => _testimonialsRepository ?? new BaseRepository<Testimonials>(_context);
         private readonly IBaseRepository<Member> _memberRepository;
         public IBaseRepository<Member> MemberRepository => _memberRepository ?? new BaseRepository<Member>(_context);

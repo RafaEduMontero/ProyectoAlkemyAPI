99329ce70034f5a922bf1a6c7cd71099609e7b30
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-30 10:21:48 -0300

    Agregando la segunda clase de serivico para User

--- OngProject/Controllers/UserController.cs
+++ OngProject/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OngProject.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
         
     }
---
+++ OngProject/Core/Services/UserService.cs
@@ -0,0 +1,13 @@
+using OngProject.Core.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Services
+{
+    public class UserService : IUserService
+    {
+        
+    }
+}

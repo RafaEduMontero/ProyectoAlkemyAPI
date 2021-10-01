11153a1e811b3728ef640c11e7bd3a9588d27163
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-01 10:52:21 -0300

    Se corrijen duplicados en la clase UnitofWork. Se ordenan campos

--- OngProject/Infrastructure/Repositories/UnitOfWork.cs
+++ OngProject/Infrastructure/Repositories/UnitOfWork.cs
@@ -10,29 +10,33 @@ namespace OngProject.Infrastructure.Repositories
 {
     public class UnitOfWork : IUnitOfWork
     {
+        #region Constructor y Contexto AppDbContext
         private readonly ApplicationDbContext _context;
 
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
-        }
+        } 
+        #endregion
 
         private readonly IBaseRepository<Organizations> _organizationsRepository;
         private readonly IBaseRepository<Category> _categoryRepository;
         private readonly IBaseRepository<Testimonials> _testimonialsRepository;
-        private readonly IBaseRepository<Activities> _ActivitiesRepository;
+        private readonly IBaseRepository<Activities> _activitiesRepository;
         private readonly IBaseRepository<Member> _memberRepository;
+        private readonly IBaseRepository<Roll> _rollRepository;
+        private readonly IBaseRepository<User> _userRepository;
+        private readonly IBaseRepository<News> _newsRepository;
 
-        public IBaseRepository<Organizations> NewsRepository => _organizationsRepository ?? new BaseRepository<Organizations>(_context);
+        public IBaseRepository<Organizations> OrganizationsRepository => _organizationsRepository ?? new BaseRepository<Organizations>(_context);
         public IBaseRepository<Category> CategoriesRepository => _categoryRepository ?? new BaseRepository<Category>(_context);
         public IBaseRepository<Testimonials> TestimonialsRepository => _testimonialsRepository ?? new BaseRepository<Testimonials>(_context);
-        private readonly IBaseRepository<Member> _memberRepository;
+        public IBaseRepository<Activities> ActivitiesRepository => _activitiesRepository ?? new BaseRepository<Activities>(_context);
         public IBaseRepository<Member> MemberRepository => _memberRepository ?? new BaseRepository<Member>(_context);
-        private readonly IBaseRepository<Activities> _ActivitiesRepository;
-        private readonly IBaseRepository<Roll> _rollRepository;
-
-        public IBaseRepository<Activities> ActivitiesRepository => _ActivitiesRepository ?? new BaseRepository<Activities>(_context);
         public IBaseRepository<Roll> RollRepository => _rollRepository ?? new BaseRepository<Roll>(_context);
+        public IBaseRepository<User> UserRpository => _userRepository ?? new BaseRepository<User>(_context);
+        public IBaseRepository<News> NewsRepository => _newsRepository ?? new BaseRepository<News>(_context);
+
 
         public void Dispose()
         {
@@ -50,7 +54,6 @@ namespace OngProject.Infrastructure.Repositories
             await _context.SaveChangesAsync();
         }
 
-        private readonly IBaseRepository<User> _userRepository;
-        public IBaseRepository<User> UserRpository => _userRepository ?? new BaseRepository<User>(_context);
+       
     }
 }

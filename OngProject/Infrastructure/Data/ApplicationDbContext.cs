9df81de628aa060975e72b9df34cad5239d661b4
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-01 11:40:17 -0300

    Se agregaron los DbSet faltantes

--- OngProject/Infrastructure/Data/ApplicationDbContext.cs
+++ OngProject/Infrastructure/Data/ApplicationDbContext.cs
@@ -14,17 +14,21 @@ namespace OngProject.Infrastructure.Data
             base.OnModelCreating(builder);
             //de esta manera la propiedad email de los usuarios sera unica y no se podra repetir
             builder.Entity<User>(entity => {
-                entity.HasIndex(e => e.email).IsUnique();
+                entity.HasIndex(e => e.Email).IsUnique();
             });
         }
+
+        public DbSet<Activities> Activities { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Comments> Comments { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }
+        public DbSet<Member> Members { get; set; }  
+        public DbSet<News> News { get; set; }   
+        public DbSet<Organizations> Organizations { get; set; }
+        public DbSet<Roll> Rolls { get; set; }  
+        public DbSet<Slides> Slides { get; set; }
         public DbSet<Testimonials> Testimonials { get; set; }
         public DbSet<User> Users { get; set; }
-        //Referencia a la Entidad : Slides
-        public DbSet<Slides> Slides { get; set; }
-        public DbSet<Organizations> Organizations { get; set; }
-        public DbSet<News> News{ get; set; }
 
     }
 }

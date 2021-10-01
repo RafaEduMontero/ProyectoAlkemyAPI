e01ef432e727087390cd8ce5ac32f84b57d75f37
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2021-10-01 01:08:49 +0000

    UnitOfWork.cs editado

--- OngProject/Infrastructure/Repositories/UnitOfWork.cs
+++ OngProject/Infrastructure/Repositories/UnitOfWork.cs
@@ -22,12 +22,14 @@ namespace OngProject.Infrastructure.Repositories
         public IBaseRepository<Organizations> NewsRepository => _organizationsRepository ?? new BaseRepository<Organizations>(_context);
         public IBaseRepository<Testimonials> TestimonialsRepository => _testimonialsRepository ?? new BaseRepository<Testimonials>(_context);
         private readonly IBaseRepository<Member> _memberRepository;
-        public IBaseRepository<Member> NewsRpository => _memberRepository ?? new BaseRepository<Member>(_context);
+        public IBaseRepository<Member> MemberRepository => _memberRepository ?? new BaseRepository<Member>(_context);
         private readonly IBaseRepository<Activities> _ActivitiesRepository;
-
-        private readonly IBaseRepository<User> _userRepository;
+        private readonly IBaseRepository<Roll> _rollRepository;
 
         public IBaseRepository<Activities> ActivitiesRepository => _ActivitiesRepository ?? new BaseRepository<Activities>(_context);
+        public IBaseRepository<Roll> RollRepository => _rollRepository ?? new BaseRepository<Roll>(_context);
+        
+        private readonly IBaseRepository<User> _userRepository;
         public IBaseRepository<User> UserRpository => _userRepository ?? new BaseRepository<User>(_context);
 
         public void Dispose()

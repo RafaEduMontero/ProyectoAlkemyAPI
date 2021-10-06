315bdea7d953bfa88854e0eb0646a2e6f70f26b6
nicolasep <nicolaseduardoperez@gmail.com>
2021-10-06 20:05:44 -0300

    Agrego paquetes de AWSS3, carpeta con clases en Helper

---
+++ OngProject/Core/Helper/S3/S3AwsHelper.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Helper.S3
+{
+    public class S3AwsHelper
+    {
+        private readonly IAmazonS3 _amazonS3;
+        public S3AwsHelper(IAmazonS3 amazonS3)
+        {
+
+        }
+    }
+}

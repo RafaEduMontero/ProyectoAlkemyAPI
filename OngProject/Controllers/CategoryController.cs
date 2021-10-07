59de07b2ffad67531f32c99584f50ac3e50caf9e
EliOlivera <eliolivera570@gmail.com>
2021-10-07 11:30:14 -0300

    agregado de autenticacion

--- OngProject/Controllers/CategoryController.cs
+++ OngProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.DTOs;
 using OngProject.Core.Interfaces.IServices;
 using System;
@@ -20,6 +21,7 @@ namespace OngProject.Controllers
             _CategoriesServices = CategoriesServices;
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CategoryNameDTO>>> Get()
         {
@@ -30,6 +32,7 @@ namespace OngProject.Controllers
             
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
--- OngProject/Controllers/MemberController.cs
+++ OngProject/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.DTOs;
 using OngProject.Core.Interfaces.IServices;
 using System;
@@ -19,6 +20,7 @@ namespace OngProject.Controllers
             _memberServices = memberServices;
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpGet]
          public async Task<IEnumerable<MembersDTO>> Get()
         {

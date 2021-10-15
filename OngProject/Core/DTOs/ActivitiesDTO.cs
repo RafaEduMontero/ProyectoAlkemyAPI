77299a1d8dee3c14158359f7a16dde1cbd624069
EliOlivera <eliolivera570@gmail.com>
2021-10-15 12:37:59 -0300

    correccion

---
+++ OngProject/Core/DTOs/ActivitiesDTO.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OngProject.Core.DTOs
+{
+    public class ActivitiesDTO
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+
+    }
+}

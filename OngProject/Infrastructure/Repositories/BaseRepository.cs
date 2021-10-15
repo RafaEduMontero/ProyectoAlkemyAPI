e0c2c18ed02ca2d8acfca7eeb9562b2868ff6521
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-14 21:03:11 -0300

    Se hacen correciones a metodo de busqueda generico, y se corrije un error en uno de los controles.

--- OngProject/Infrastructure/Repositories/BaseRepository.cs
+++ OngProject/Infrastructure/Repositories/BaseRepository.cs
@@ -22,7 +22,6 @@ namespace OngProject.Infrastructure.Repositories
             _entity = context.Set<T>();
         }
         #endregion
-
         public async Task<IEnumerable<T>> GetAll()
         {
             var response = await _entity.Where(x => x.IsDeleted == false).ToListAsync();
@@ -70,15 +69,42 @@ namespace OngProject.Infrastructure.Repositories
         #endregion
         public bool EntityExists(int id)
         {
-            return _entity.Any(x => x.Id==id && x.IsDeleted == false);
+            return _entity.Any(x => x.Id == id && x.IsDeleted == false);
         }
-        public async Task<T> FindByCondition(Expression<Func<T, bool>> predicate)
-        {
-            var response = await _entity
-                .Where(x=> x.IsDeleted==false)
-                .FirstOrDefaultAsync(predicate);
 
-            return response;
+        #region 
+        /// <summary>
+        /// Metodo para realizar una busqueda por medio de una expresion LINQ
+        /// </summary>
+        /// <remarks>
+        /// <para>Ejemplo:
+        /// var request= _unitOfWork.(repositorioDeLaEntidad).FindByCondition(x=> x.campo del objeto de la Db == Campo contra el que quiero comparar);</para>
+        /// <para>Cuando se quiere agregar una entidad ligada por una FK, se debe de incluir despues de la frase, con una coma</para>
+        /// <para>Ejemplo: .FindByCondition(query LINQ, y=> y.EntidadVinculadaPorunaFK)</para>
+        /// <para> Ejemplo concreto:
+        /// var request= _unitOfWork.UsersRepository.FindByCondition(x=> x.Email == email, y=> y.Role);</para>
+        /// <para> Esto me va a traer un IEnumerable de User, y en el campo Role va a traer el Rol asignado a ese Usuario.
+        /// De ahi solamente restaria aplicar logica para hacerlo lista, o para lo que necesiten</para>
+        /// 
+        /// </remarks>
+        /// <returns>
+        /// IEnumerable de la entidad que se este ocupando, filtrada. Pueden agregarse mas propiedades. 
+        /// Por defecto trae solo las que IsDeleted==false
+        /// </returns> 
+        #endregion
+        public async Task<IEnumerable<T>> FindByCondition(Expression<Func<T, bool>> predicate,
+                                                          params Expression<Func<T, object>>[] includeProperties)
+        {
+            var response = _entity
+                .Where(x => x.IsDeleted == false)
+                .Where(predicate);
+
+            foreach (var includeProperty in includeProperties)
+            {
+                response = response.Include(includeProperty);
+            }
+
+            return await response.ToListAsync();
 
         }
         public async Task<User> GetByEmail(string email)

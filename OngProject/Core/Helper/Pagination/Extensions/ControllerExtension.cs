57a3b35216e61c5768b83f430bbbe38d86b15a4d
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-19 17:04:20 -0300

    Se agrega Paginacion para Miembros. Se agrega en un controlador aparte, para no pisar codigo del GetAll

---
+++ OngProject/Core/Helper/Pagination/Extensions/ControllerExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Helper.Pagination.Extensions
+{
+    public static class ControllerExtension
+    {
+        internal static PaginationDTO<T> AddPageLinks<T>
+            (this ControllerBase controller, string route, int page, PaginationDTO<T> response)
+            where T : class
+        {
+            if (response.TotalPages==0)
+            {
+                return response;
+            }
+
+            string urlBase = string.Concat(controller.Request.Scheme, "://", controller.Request.Host.ToUriComponent());
+            if (response.CurrentPage > 1)
+            {
+                var prevRoute = controller.Url.RouteUrl(route, new { page = page - 1 });
+
+                response.AddResourceLink(LinkedResourceType.Prev, urlBase + prevRoute);
+            }
+
+            if (response.CurrentPage < response.TotalPages)
+            {
+                var nextRoute = controller.Url.RouteUrl(route, new { page = page + 1 });
+
+                response.AddResourceLink(LinkedResourceType.Next, urlBase + nextRoute);
+            }
+
+            return response;
+        }
+
+    }
+}
--- OngProject/Core/Interfaces/IServices/IMemberServices.cs
+++ OngProject/Core/Interfaces/IServices/IMemberServices.cs
@@ -5,15 +5,15 @@ using System.Threading.Tasks;
 using OngProject.Common;
 using OngProject.Core.DTOs;
 using OngProject.Core.Entities;
+using OngProject.Core.Helper.Pagination;
 
 namespace OngProject.Core.Interfaces.IServices
 {
     public interface IMemberServices
     {
         Task<IEnumerable<MembersDTO>> GetAll();
-
         Task<Member> Insert(MembersInsertarDTO membersInsertarDTO);
-        
-        Task <Result> Delete(int id);
+        Task<Result> Delete(int id);
+        Task<PaginationDTO<MembersDTO>> GetByPage(int page);
     }
 }

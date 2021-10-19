c4099a9c4e017155e6a9ddbbce7aadee8b89d1ad
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-19 17:04:20 -0300

    Se agrega Paginacion para Miembros. Se agrega en un controlador aparte, para no pisar codigo del GetAll

---
+++ OngProject/Core/Helper/Pagination/ILinkedResource.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace OngProject.Core.Helper.Pagination
+{
+    public interface ILinkedResource
+    {
+        public IDictionary<LinkedResourceType, LinkedResource> Links { get; set; }
+    }
+}

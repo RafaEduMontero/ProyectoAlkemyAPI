61a96cfb502426ea2356512d2df7b3e73f8ba267
nicolasep <nicolaseduardoperez@gmail.com>
2021-10-20 00:14:19 -0300

    Agrego Endpoint Delete a News

--- OngProject/Core/Services/NewsServices.cs
+++ OngProject/Core/Services/NewsServices.cs
@@ -1,6 +1,8 @@
+using OngProject.Common;
 using OngProject.Core.DTOs;
 using OngProject.Core.Entities;
 using OngProject.Core.Interfaces.IServices;
+using OngProject.Core.Interfaces.IServices.AWS;
 using OngProject.Core.Mapper;
 using OngProject.Infrastructure.Repositories.IRepository;
 using System.Threading.Tasks;
@@ -11,9 +13,11 @@ namespace OngProject.Core.Services
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public NewsServices(IUnitOfWork unitOfWork)
+        private readonly IImageService _imageServices;
+        public NewsServices(IUnitOfWork unitOfWork, IImageService imageServices)
         {
             _unitOfWork = unitOfWork;
+            _imageServices = imageServices;
         }
 
         public async Task<NewsDTO> GetById(int id)
@@ -39,6 +43,28 @@ namespace OngProject.Core.Services
             return news;
 
 
+        }
+
+        public async Task<Result> Delete(int id)
+        {
+            var news = await _unitOfWork.NewsRepository.GetById(id);
+            if (news == null)
+            {
+                return new Result().NotFound();
+            }
+            var url = news.Image;
+            var result = await _unitOfWork.NewsRepository.Delete(id);
+            await _unitOfWork.SaveChangesAsync();
+            if (result != null)
+            {
+                if(url != null)
+                {
+                    await _imageServices.Delete(url);
+                }
+                
+                return new Result().Success("Novedad eliminada con exito");
+            }
+            return new Result().Fail("Ocurrio un error al eliminar la novedad");
         }
     }
 }

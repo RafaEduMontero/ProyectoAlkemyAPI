cc89b5037cd26eefbec233e9d30f1d7c755e47ba
EliOlivera <eliolivera570@gmail.com>
2021-10-21 19:06:07 -0300

    paginado de categorias

--- OngProject/Core/Interfaces/IServices/ICategoriesServices.cs
+++ OngProject/Core/Interfaces/IServices/ICategoriesServices.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using OngProject.Common;
 using OngProject.Core.DTOs;
 using OngProject.Core.Entities;
+using OngProject.Core.Helper.Pagination;
 
 namespace OngProject.Core.Interfaces.IServices
 {
@@ -14,6 +15,7 @@ namespace OngProject.Core.Interfaces.IServices
         Task<CategoryDTO> GetById(int id);
         bool EntityExist(int id);
         Task<IEnumerable<CategoryNameDTO>> GetAll();
+        Task<PaginationDTO<CategoryDTO>> GetByPage(string route,int page);
         Task<Category> Post(CategoryDTO categoryDTO);
         Task<Result> Update(int id, UpdateCategoryDTO updateCategoryDTO);
         Task<Result> Delete(int id);

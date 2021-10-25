7e3a2ee0399d69b682ca64c05caac81e1dc9e5ab
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-25 16:11:53 -0300

    Se agrega documentacion pertinente a Member Controller, junto a correciones y ordenamiento de los DTOS para que siga un mejor orden.

--- OngProject/Core/Helper/Pagination/PaginationDTO.cs
+++ OngProject/Core/Helper/Pagination/PaginationDTO.cs
@@ -8,11 +8,35 @@ namespace OngProject.Core.Helper.Pagination
 {
     public class PaginationDTO<T>
     {
-        public int CurrentPage { get; init; }    
-        public int TotalItems { get; init; }    
-        public int TotalPages { get; init; }    
+        /// <summary>
+        /// Pagina que se esta visualizando
+        /// </summary>
+        public int CurrentPage { get; init; }
+        
+        /// <summary>
+        /// Numero total de entradas que hay en la Db
+        /// </summary>
+        public int TotalItems { get; init; }
+        
+        /// <summary>
+        /// Numero total de paginas en Db. Cambia con respecto a la cantidad de 
+        /// elementos por pagina que se piden (por default, =10)
+        /// </summary>
+        public int TotalPages { get; init; }
+        
+        /// <summary>
+        /// Url con la pagina previa
+        /// </summary>
         public string PrevPage { get; init; }
+        
+        /// <summary>
+        /// Url con la pagina siguiente
+        /// </summary>
         public string NextPage { get; init; }
+
+        /// <summary>
+        /// Lista de elementos por pagina
+        /// </summary>
         public List<T> Items { get; init; }
     }
 }

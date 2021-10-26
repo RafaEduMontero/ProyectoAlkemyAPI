8dadc4449bf46a6faaafdd8f5e5ddc7509ec2877
EliOlivera <eliolivera570@gmail.com>
2021-10-25 23:26:13 -0300

    Agregado de documentacion de comments

--- OngProject/Core/DTOs/CommentUpdateDTO.cs
+++ OngProject/Core/DTOs/CommentUpdateDTO.cs
@@ -7,8 +7,17 @@ namespace OngProject.Core.DTOs
 {
     public class CommentUpdateDTO
     {
+        /// <summary>
+        /// Id del usuario a modificar
+        /// </summary>
         public int? UserId { get; set; }
+          /// <summary>
+        /// Id de la Noticia a modificar
+        /// </summary>
         public int? NewId { get; set; }
+        /// <summary>
+        /// Comentario a modificar
+        /// </summary>
         public string Body { get; set; }
     }
 }
--- OngProject/Core/Interfaces/IServices/ICommentsServices.cs
+++ OngProject/Core/Interfaces/IServices/ICommentsServices.cs
@@ -1,5 +1,6 @@
 ﻿using OngProject.Common;
 using OngProject.Core.DTOs;
+using OngProject.Core.Helper.Pagination;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,12 @@ namespace OngProject.Core.Interfaces.IServices
     public interface ICommentsServices
     {
         Task<IEnumerable<CommentsDTO>> GetAll();
+        Task<PaginationDTO<CommentsDTO>> GetByPage(string route,int page);
         Task<IEnumerable<CommentsDTO>> GetById(int id);
         bool EntityExists(int id);
         Task<NewCommentsDTO> Insert(NewCommentsDTO newCommentsDTO);
         Task<Result> Delete(int id);
         Task<bool> ValidateCreatorOrAdmin(ClaimsPrincipal user, int id);
-        Task<Result> Update(int id, CommentUpdateDTO commentsDTO);
+        Task<Result> Update(int id, CommentUpdateDTO CommentUpdateDTO);
     }
 }
